10822498f9e9af492236ee9d81600392764fceb8
Chris Weber <chris791w@gmail.com>
2014-11-07 12:33:55 -0500

    Updated Android makefile

---
+++ jni/Android.mk
@@ -0,0 +1,17 @@
+LOCAL_PATH := $(call my-dir)
+
+#for compiling the shared library
+include $(CLEAR_VARS)
+LOCAL_MODULE    := android_ptrace
+LOCAL_CFLAGS    := -Werror
+LOCAL_SRC_FILES := android_ptrace.c
+LOCAL_LDLIBS    := -llog 
+include $(BUILD_SHARED_LIBRARY)
+
+
+#for compiling the client, need to add the library as a dependency?
+include $(CLEAR_VARS)
+LOCAL_MODULE := ptrace_client
+LOCAL_SRC_FILES := ptrace_client.c
+LOCAL_LDLIBS := -L$(SYSROOT)/usr/lib -llog
+include $(BUILD_EXECUTABLE)
